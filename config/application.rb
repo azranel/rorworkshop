3974bd0dc975d2aca54086034bc2a84d0c5f741e
azranel <chunkyo@gmail.com>
2014-08-09 13:32:58 +0200

    fixed production env

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,4 @@ module Workshops
     # config.i18n.default_locale = :de
   end
 
-  ActionDispatch::Callbacks.after do
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
-
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
-  end
-end
 end
